87251412f5f0f5ff9d60000e5b8528f6db134935
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-03-08 11:43:01 -0300

    .

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ var nome_Usuario = prompt('Qual é o seu nome?');
 var texto = document.querySelector('span');
 
 texto.textContent = nome_Usuario;
+
+while(nome_Usuario == ''){
+        var nome_Usuario = prompt('Qual é o seu nome?');       
+}

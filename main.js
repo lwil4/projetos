c0ec7977a3e421fa7082c2cdcbaec8ff6459678e
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-03-08 12:15:06 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 var nome_Usuario = prompt('Qual é o seu nome?');
 var texto = document.querySelector('span');
 
-while(nome_Usuario == '' ){// || nome_Usuario == null)
-        var nome_Usuario = prompt('Qual é o seu nome?');       
+while(nome_Usuario == '' ){// || nome_Usuario == null
+nome_Usuario = prompt('Qual é o seu nome?');       
 }
 
-if(nome_Usuario == null){
+if(nome_Usuario == null){ 
 texto.textContent = 'usuário'
 }else{
-texto.textContent = nome_Usuario;
+texto.textContent = nome_Usuario;//se a resposta da caixinha der null, ele ira repetir o código até que haja uma resposta válida
 }
-
-texto.textContent = nome_Usuario;
+//linha de respiro para garantia de funcionamento do código

0f8cb79bdd2b99e09a426a966faa024caedca90d
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-04-18 09:56:09 -0300

    ds

--- cont.js
+++ cont.js
@@ -17,4 +17,4 @@ const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date();
 let tempoObjetivo1 = new Date('2024-04-19T00:00:00');
 
-contadores[0].textContent = "olá visitante";
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
